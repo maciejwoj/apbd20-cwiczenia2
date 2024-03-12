6a689e8076f5620f4ee7207a8deece0e7d215f32
s27578 <maciek1woj@gmail.com>
2024-03-12 16:41:26 +0100

    delete some lines and changes some

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,6 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("something change");
+Console.WriteLine("second change");
 
-Console.WriteLine("zmiana1");
-Console.WriteLine("Hello, World!");
