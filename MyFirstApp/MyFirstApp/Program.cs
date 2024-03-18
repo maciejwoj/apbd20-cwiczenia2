b84ce2931045d3bfba2a2dba85a5d6a8876a1927
maciejwoj <maciek1woj@gmail.com>
2024-03-18 23:55:00 +0100

    merge average feature

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,6 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("something change");
-Console.WriteLine("change");
-Console.WriteLine("third change ");
+static double GetAverage(int[] numbers)
+{
+    double sum = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        sum += numbers[i];
+    }
 
+    return sum / numbers.Length;
+}
+
+int[] numbers = { 1, 2, 3, 4 };
+double average = GetAverage(numbers);
+Console.WriteLine(average);
